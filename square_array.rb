b526b8d5a064d868f785b7f04570f78053ad19df
Amy Fogelman <abfnj1@gmail.com>
2019-06-02 10:34:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(numbers)
-  # your code here	  new_numbers = []
-end 	  numbers.each do |num|
+  # your code here	  
+  new_numbers = []
+end 	  
+numbers.each do |num|
     new_numbers << (num ** 2)
   end
   return new_numbers
